94df66a7996254ac382c02185cc3b65c2b21a644
AMAL DEEP <161101203+Amaldeep98@users.noreply.github.com>
2025-09-21 20:07:01 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,13 +4,4 @@ set -e
 PORT=8088
 
 # Find container using the port
-CONTAINER_ID=$(docker ps -q --filter "publish=$PORT")
-
-if [ -n "$CONTAINER_ID" ]; then
-    echo "Stopping container(s) using port $PORT..."
-    docker stop $CONTAINER_ID
-    echo "Removing container(s)..."
-    docker rm $CONTAINER_ID
-else
-    echo "No container is using port $PORT."
-fi
+docker stop memory-monster
