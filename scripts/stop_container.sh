4b17cfc87d3732f7c0a1dcdfc377af5a4867399e
AMAL DEEP <161101203+Amaldeep98@users.noreply.github.com>
2025-09-21 20:50:43 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-# Find any running container (by name or all containers).
-containerid=$(docker ps -q -f "name=my_app")  
+docker stop memory-monster &> /dev/null
 
-if [ -n "$containerid" ]; then
-    echo "Stopping and removing container $containerid"
-    docker rm -f "$containerid"
-else
-    echo "No existing container to stop."
-fi
+
+docker rm memory-monster &> /dev/null
+
