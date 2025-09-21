915c93e3849acb9b6674dec79550a2fe51051104
AMAL DEEP <161101203+Amaldeep98@users.noreply.github.com>
2025-09-21 18:25:38 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull amaldeep98/memory-monster:latest
 
 # Run the Docker image as a container
-run run -d -p 8088:8088 amaldeep98/memory-monster:latest
+docker run -d -p 8088:8088 amaldeep98/memory-monster:latest
